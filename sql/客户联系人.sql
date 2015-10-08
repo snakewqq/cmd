1d44206cadf3b3db589d85d3661c559cecef2200
Wesley Liu <wliu@capvision.com>
2015-10-08 15:23:26 +0800

    add php-pdo-pgsql

--- sql/客户联系人.sql
+++ sql/客户联系人.sql
@@ -1,6 +1,7 @@
 UPDATE ndb_contact_others
 SET `value` = REPLACE(`value`,'@opentide.com','@thecorebridge.com')
 
+
 CREATE TABLE tmp_0915(
 id INT NOT NULL,
 oldvalue VARCHAR(100) NULL,
@@ -8,7 +9,7 @@ newvalue VARCHAR(100) NULL
 )
 
 INSERT INTO tmp_0915
-SELECT co.id,co.value,REPLACE(co.value,'@opentide.com','@thecorebridge.com') AS newvalue
+SELECT co.id,co.value,REPLACE(co.value,'@thecorebridge.com','@samsung.com') AS newvalue
 FROM ndb_contact_others co
 INNER JOIN ndb_contacts c ON c.contactid = co.id
 INNER JOIN ndb_client_contact cc ON cc.id = c.objectid
@@ -19,9 +20,10 @@ AND cc.clientid =134495
 
 SELECT * 
 
-UPDATE ndb_contact_others
-SET `value` = a.newvalue
-FROM tmp_0915 a
-INNER JOIN ndb_contact_others b ON a.id = b.id
+UPDATE ndb_contact_others,tmp_0915
+SET `value` = tmp_0915.newvalue
+WHERE  ndb_contact_others.id = tmp_0915.id
 
-DELETE TABLE tmp_0915
+SELECT * FROM tmp_0915
+
+DROP TABLE tmp_0915

f476452a03f880f90da4d99e17b821013edd6def
Wesley Liu <wliu@capvision.com>
2015-10-13 08:08:51 +0800

    add sql.

---
+++ sql/修改project client.sql
@@ -0,0 +1,8 @@
+SELECT * FROM ndb_project_client WHERE projectid = 157290;
+
+UPDATE ndb_project_client
+SET clientid = 134534
+WHERE projectid = 157290;
+
+DELETE FROM ndb_project_client_contact WHERE id = 36560;
+
---
+++ sql/绑定官网和db项目.sql
@@ -0,0 +1,7 @@
+SELECT * FROM capweb_event WHERE id = 1137
+官网http://www.capvision.com/conference/1137 →DB项目 http://db.capvision.com/project/conference/INDEX/tasklist?id=157707
+官网http://www.capvision.com/conference/1138 →DB项目 http://db.capvision.com/project/conference/INDEX/tasklist?id=157708
+
+UPDATE capweb_event 
+SET ndbrelateid = 157708
+WHERE id = 1138

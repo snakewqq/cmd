4bd0c5ca35eef0a1d03f71b8c424063621cdb94b
Wesley Liu <wliu@capvision.com>
2015-10-20 20:03:46 +0800

    add nagios

---
+++ sql/ksh修改礼品状态.sql
@@ -0,0 +1,3 @@
+UPDATE vesta_credits_goods 
+SET actual = 1
+WHERE id BETWEEN 174 AND 183;
---
+++ sql/ndb修改task为arrange.sql
@@ -0,0 +1,5 @@
+SELECT * FROM ndb_project_task WHERE id = 337369
+
+UPDATE ndb_project_task
+SET `status` = 7
+WHERE id = 337369

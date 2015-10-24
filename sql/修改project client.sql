dc137177b65051f74873f859938c7bc2f0ca4d6a
Wesley Liu <wliu@capvision.com>
2015-10-24 14:14:36 +0800

    add sql.

--- sql/修改project client.sql
+++ sql/修改project client.sql
@@ -1,8 +1,14 @@
 SELECT * FROM ndb_project_client WHERE projectid = 157290;
 
+/*修改项目所属客户*/
 UPDATE ndb_project_client
-SET clientid = 134534
-WHERE projectid = 157290;
+SET clientid = 135040
+WHERE projectid = 143069;
 
-DELETE FROM ndb_project_client_contact WHERE id = 36560;
 
+SELECT * FROM ndb_project_client_contact WHERE projectid = 143069
+
+UPDATE ndb_project_client_contact 
+SET contactid = 139727,
+    contact_newid = 139727
+WHERE projectid = 143069
